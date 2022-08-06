d54d53754e4815a7b07b219c2317eba25c93bc92
RickyKT <s3783560@student.rmit.edu.au>
2022-08-06 15:40:01 +1000

    rounded buttons links

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,15 +8,14 @@ function Navbar () {
     return (
         <nav className={"navbar navbar-expand-lg navbar-dark bg-dark "}>
             <div className = {"container-fluid"}>
-
                 <Link to= "/" className = "navbar-brand p-2" ><h1 className = "display-5">Loop Agile Now</h1></Link>
 
                 <ul className={"nav justify-content-between"}>
                     <li className="nav-item">
-                        <Link to= "sign_in" className = {"nav-link p-3"} ><button type = "button" class = "btn btn-light btn-lg"><h6 className = "display-7">Sign In</h6></button></Link>
+                        <Link to= "sign_in" className = {"nav-link p-3"} ><button type = "button" class = "btn btn-light btn-lg btn-rounded"><h6 className = "display-7">Sign In</h6></button></Link>
                     </li>
                     <li className={"nav-item"}>
-                        <Link to= "sign_up" className = {"nav-link p-3"} > <button type = "button" class = "btn btn-light btn-lg"><h6 className = "display-7">Sign Up</h6></button></Link>
+                        <Link to= "sign_up" className = {"nav-link p-3"} > <button type = "button" class = "btn btn-light btn-lg btn-rounded"><h6 className = "display-7">Sign Up</h6></button></Link>
                     </li>
                 </ul>
             </div>
