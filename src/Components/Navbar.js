b3a241e11267e71cfbce87c888a84b5247992f47
Nick-jw <s3920382@student.rmit.edu.au>
2022-08-25 21:25:19 +1000

    formatting

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,19 +10,17 @@ function Navbar ({loggedInUser, onLogout}) {
         <nav className={"navbar navbar-expand-lg navbar-dark bg-dark sticky-top"}>
             <div className = {"container-fluid "}>
                 {loggedInUser ? ( //user is logged in display profile/feed/log out button; unacitve h1
-                        <><Link to= "/" className = "navbar-brand p-2" ><h1 className = "display-5">Loop Agile Now</h1></Link>
+                    <><Link to= "/" className = "navbar-brand p-2" ><h1 className = "display-5">Loop Agile &#x25AA; <strong><em>Now</em></strong></h1></Link>
                     <ul className={"nav justify-content-between"}>
                         <li className="nav-item">
                             <div className = {"nav-link p-3"} >
                                 <button type = "button" className = "btn btn-light btn-lg btn-rounded"
                                 onClick={() => {
                                     navigate("/profile", {replace : true,})
-
                                 }}
                                 >
                                     <h6 className = "display-7">Profile</h6>
                                 </button>
-                                
                             </div>
                         </li>
                         <li className="nav-item">
@@ -54,7 +52,7 @@ function Navbar ({loggedInUser, onLogout}) {
                         </li>
                 </ul></>
                 ) : ( //user is not loggedin sign up/ sign in button; acitve h1
-                    <><Link to= "/" className = "navbar-brand p-2" ><h1 className = "display-5">Loop Agile Now</h1></Link>
+                    <><Link to= "/" className = "navbar-brand p-2" ><h1 className = "display-5">Loop Agile &#x25AA; <strong><em>Now</em></strong></h1></Link>
                     <ul className={"nav justify-content-between"}>
                     <li className="nav-item">
                         <Link to= "sign_in" className = {"nav-link p-3"} ><button type = "button" class = "btn btn-light btn-lg btn-rounded"><h6 className = "display-7">Sign In</h6></button></Link>
