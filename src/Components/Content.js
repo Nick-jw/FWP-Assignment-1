902194fa3b42b7340df0086ffc37124f4bb14109
Nick-jw <s3920382@student.rmit.edu.au>
2022-08-25 22:12:06 +1000

    card styling

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -16,21 +16,23 @@ function Content () {
                     {/*Empty column for the left sidebar */}
                     <div className={"col-sm-1"}></div>
                         
-                    {/* This div contains all main content*/}
+                    {/* This div contains all main content hello world*/}
                     <div className={"col-sm-10 bg-light text-center"} style={{minHeight:500}}>
                         <br/>
                         <p className='display-1'>Loop Agile &#x25AA; <strong><em>Now</em></strong></p>
 
                         <blockquote className={"h3 text-center"}>Create an account to start linking!</blockquote>
                         <br/>
-                        
+
+                        <div className='separator' style={{height:50}}></div>
+
                         {/*Card 1*/}
-                        <div className= 'card'>
+                        <div className= 'card border-0'>
                             <div className = 'row g-0'>
                                 <div className = 'col-md-4'>
                                     <img className = 'card-img img-fluid ' src = {pic1} alt='bg'/>
                                 </div>
-                                <div className = 'col-md-8 text-center'>
+                                <div className = 'col-md-8 d-flex align-items-center'>
                                     <div className = 'card-body'>
                                         <p className = 'card-text display-5 text-center'>Collaborating with colleages is now made simplier and easy to use.</p>
                                     </div>
@@ -41,10 +43,10 @@ function Content () {
                         <br/>
 
 
-                        {/* Card 2*/}
-                        <div className= 'card'>
+                        {/* Card 2 */}
+                        <div className= 'card border-0'>
                             <div className = 'row g-0'>
-                                <div className = 'col-md-8'>
+                                <div className = 'col-md-8 d-flex align-items-center'>
                                     <div className = 'card-body'>
                                         <p className = 'card-text display-5 text-center'>See what your colleages are up to via the feed. Whether it be work related or personal.</p>
                                     </div>
@@ -55,7 +57,8 @@ function Content () {
                             </div>
                         </div>
                         <br/>
-                        <br/>
+
+                        <div className='space' style={{height:50}}></div>
 
 
                     
