ae19992aab7f45a1cd0e9ca2382532c99e8b7c6c
RickyKT <s3783560@student.rmit.edu.au>
2022-08-28 17:11:59 +1000

    not working

--- src/Components/Update.js
+++ src/Components/Update.js
@@ -4,36 +4,20 @@ import {useNavigate} from 'react-router-dom'
 
 function Update () {
     const navigate = useNavigate()
-
-
-    //getting user list
-    let user_list = JSON.parse(localStorage.getItem('users'))
-    let curr_email = JSON.parse(localStorage.getItem('loggedInUser'))
-    let curr_user_index = 0
-
-     //getting current name, email password
     
-     for (var i = 0; i < user_list.length; i++) {
-        if (user_list[i]['email'] === JSON.parse(localStorage.getItem('loggedInuser'))) {
-            curr_user_index = i
-            break
-        }
-     }
-    // logged in username and password for modification
-    let curr_username = user_list[curr_user_index]['username']
-    let curr_password = user_list[curr_user_index]['password']
-    let curr_date = user_list[curr_user_index]['date']
+    const[data, setData] = useState(localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')):[])
 
-   
 
-    const [form,setForm] = useState({})
+    const [form,setForm] = useState({
+
+    });
 
     const handleChange = (field) => (event) => {
         setForm((form) => ({...form, [field]: event.target.value}));
     }
 
-    const[data, setData] = useState(localStorage.getItem('users') ? JSON.parse(localStorage.getItem('users')):[])
-    console.log(data)
+    
+
     useEffect(() => {
         localStorage.setItem('users',JSON.stringify(data));
     }, [data]);
@@ -41,23 +25,32 @@ function Update () {
 
     const onSubmit = (event) => {
         event.preventDefault()
-        setData ((data) =>{
+        console.log('7')
+        //not accessing set Data function WTF
+        setData (data =>{
+            
             let newData = [...data];
 
             newData = newData.map ((entry) => {
                 if (entry.email === form.email) {
+                    
                     return form;
+                    
                 }
                 else{
+                    
                     return entry;
                 }
-                
+            
                 
             })
+
+            
             return newData
         });
         
         navigate('/profile', {replace:true,})
+        console.log('8')
 
     }
 
@@ -78,17 +71,17 @@ function Update () {
                                 <form onSubmit = {onSubmit}>
                                     <div>
                                         <label className = 'col-sm-2 col-form-label h6'>Name</label>
-                                        <input className = 'form-control' type = 'text' placeholder = 'Username' value ={form.username} onChange = {handleChange('username')}/>
+                                        <input className = 'form-control' type = 'text' placeholder = 'username' value ={form.username} onChange = {handleChange('username')}/>
 
                                     </div>
                                     <div>
                                     <label className = 'col-sm-2 col-form-label h6'>Email</label>
-                                    <input className = 'form-control' type = 'email' placeholder = 'Email' value = {form.email} onChange =  {handleChange('email')}/>
+                                    <input className = 'form-control' type = 'email' placeholder = 'email' value = {form.email} onChange =  {handleChange('email')}/>
                                     </div>
 
                                     <div>
                                     <label className = 'col-sm-2 col-form-label h6'>Password</label>
-                                    <input className = 'form-control' type = 'password' placeholder = 'Password' value = {form.password} onChange =  {handleChange('password')}/>
+                                    <input className = 'form-control' type = 'password' placeholder = 'password' value = {form.password} onChange =  {handleChange('password')}/>
                                     </div>
 
                                     <div className = 'container d-flex justify-content-between'>
@@ -98,7 +91,7 @@ function Update () {
                                         }}>
                                             Cancel
                                         </button>
-                                        <button type = 'submit' className = 'btn btn-lg btn-info btn-rounded  m-3'
+                                        <button type = 'submit'  className = 'btn btn-lg btn-info btn-rounded  m-3'
                                         >
                                             Save changes
                                         </button>
